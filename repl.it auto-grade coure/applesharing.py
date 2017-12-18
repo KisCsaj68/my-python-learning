8a04d8adb3bc187445438d98a62ea74d0949db64
KisCsaj68 <34596151+KisCsaj68@users.noreply.github.com>
2017-12-18 08:43:50 +0100

    Create applesharing.py
    Lesson about:
     // - # double forward slash is an integer division
            # it returns only the quotient of the division (i.e. no remainder)
    % - # percent sign is a modulus operator
            # it gives the remainder of the left value divided by the right value

---
+++ repl.it auto-grade coure/applesharing.py
@@ -0,0 +1,10 @@
+# Read the numbers like this:
+# n = int(input())
+
+# Print the result with print()
+
+# Example of division, integer division and remainder:
+N = int(input())
+K = int(input())
+print(K//N)
+print(K%N)
